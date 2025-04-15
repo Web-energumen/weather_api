1bf68fd4c038f61841cd593192e68c54d2cb7020
Alex <doroshevsn@gmail.com>
2025-04-15 13:23:55 +0300

    Add DRF throttle settings for rate limiting

--- weather_api/settings.py
+++ weather_api/settings.py
@@ -150,3 +150,14 @@ CACHES = {
         }
     }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/minute',
+        'user': '100/minute',
+    }
+}
