c708ab9c144ef50e4579c1a62841ad8ae275ae87
Alex <97349499+Web-energumen@users.noreply.github.com>
2025-04-15 12:38:24 +0300

    Update urls.py

--- weather_api/urls.py
+++ weather_api/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('weather.urls'))
+    path('api/', include('weather.urls'))
 ]
